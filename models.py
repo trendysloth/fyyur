94bc5d118396214d99f5bbd303bb2ecb342d7383
trendysloth <ww296@cornell.edu>
2020-04-26 16:33:01 -0700

    formatting code style

--- models.py
+++ models.py
@@ -1,16 +1,24 @@
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
+
 class Show(db.Model):
     __tablename__ = 'shows'
 
     id = db.Column(db.Integer, primary_key=True)
-    artist_id = db.Column(db.Integer, db.ForeignKey('artists.id'), nullable=False)
-    venue_id = db.Column(db.Integer, db.ForeignKey('venues.id'), nullable=False)
+    artist_id = db.Column(
+        db.Integer,
+        db.ForeignKey('artists.id'),
+        nullable=False)
+    venue_id = db.Column(
+        db.Integer,
+        db.ForeignKey('venues.id'),
+        nullable=False)
     start_time = db.Column(db.DateTime, nullable=False)
 
     def __repr__(self):
-      return f'<Show {self.venue_id} {self.artist_id} {self.start_time}>'
+        return f'<Show {self.venue_id} {self.artist_id} {self.start_time}>'
+
 
 class Venue(db.Model):
     __tablename__ = 'venues'
@@ -31,7 +39,8 @@ class Venue(db.Model):
     shows = db.relationship('Show', backref='venues', lazy=True)
 
     def __repr__(self):
-      return f'<Venue {self.id} {self.name} {self.city}>'
+        return f'<Venue {self.id} {self.name} {self.city}>'
+
 
 class Artist(db.Model):
     __tablename__ = 'artists'
@@ -48,6 +57,6 @@ class Artist(db.Model):
     seeking_venue = db.Column(db.Boolean, default=False)
     seeking_description = db.Column(db.String(120))
     shows = db.relationship('Show', backref='artists', lazy=True)
-    
+
     def __repr__(self):
-      return f'<Artist {self.id} {self.name}>'
+        return f'<Artist {self.id} {self.name}>'
